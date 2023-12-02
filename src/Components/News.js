39b6fe010610ff69f3e35bcfcff05ec7a77e327b
Babar-Amjad <babarkhokhr0@gmail.com>
2023-12-02 04:00:39 -0800

    Fixed articles Length

--- src/Components/News.js
+++ src/Components/News.js
@@ -68,7 +68,7 @@ const News = (props) => {
         {loading && <Spinner />}
       </div>
       <InfiniteScroll
-        dataLength={articles.length}
+        dataLength={articles?.length}
         next={fetchMoreData}
         hasMore={articles.length !== totalResults}
         loader={<Spinner />}
